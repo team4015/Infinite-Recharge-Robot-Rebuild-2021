33255c756caa300126a45237a694bab8df95a418
Shane Pinto <shanepinto18@yahoo.com>
2021-01-15 22:44:43 -0500

    Drivetrain Code
    Basic differential drive train code.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,15 +1,26 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
-public final class Constants {}
+public final class Constants
+{
+    public static final int LEFT_MOTOR_FRONT = 3;
+    public static final int LEFT_MOTOR_REAR = 5;
+    public static final int RIGHT_MOTOR_FRONT = 4;
+    public static final int RIGHT_MOTOR_REAR = 8;
+
+    public static final int INTAKE_CAN_SPARK = 8; 
+    public static final int CONVEYOR_SPARK = 9;
+    public static final int SHOOTER_VICTOR = 2;
+
+    public static final int CONTROLLER_XBOX_PORT_DRIVER = 1;
+    public static final int CONTROLLER_XBOX_PORT_SUBSYSTEM = 0;
+
+    //PISTONS
+    public static final int SHOOTER_PISTON_DEPLOY = 2;
+    public static final int SHOOTER_PISTON_RETRACT = 3;
+    public static final int INTAKE_PISTON_DEPLOY = 7;
+    public static final int INTAKE_PISTON_RETRACT = 6;
+    public static final int CLIMBER_PISON_DEPLOY = 1; //tbd
+    public static final int CLIMBER_PISON_RETRACT = 0;
+    //RING LIGHT
+    public static final int RING_LIGHT = 5;//tbd
+}
