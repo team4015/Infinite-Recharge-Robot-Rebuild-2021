9bb8f3d2d8bc4ad37ad48532aac98ded5706faa5
droid-101 <49104581+droid-101@users.noreply.github.com>
2021-07-27 15:46:10 -0400

    Conveyor Standby Mode (#23)
    - Conveyor subsystem will now auto load balls when no user input is given
    - Conveyor will no longer feed balls if a ball has reached the shooter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,8 +21,8 @@ public final class Constants
     public static final int RETRACT_INTAKE = 4;
     public static final int SPIN_INTAKE = 1;
     public static final int REVERSE_INTAKE = 3;
-    public static final int FEED_CONVEYOR = 3;
-    public static final int REVERSE_CONVEYOR = 4;
+    public static final int FEED_CONVEYOR = 4;
+    public static final int REVERSE_CONVEYOR = 3;
     public static final int CHARGE_SHOOTER = 1;
 
     // PISTONS
@@ -30,8 +30,13 @@ public final class Constants
     public static final int SHOOTER_PISTON_RETRACT = 3;
     public static final int INTAKE_PISTON_DEPLOY = 7;
     public static final int INTAKE_PISTON_RETRACT = 6;
-    public static final int CLIMBER_PISON_DEPLOY = 1; //tbd
+    public static final int CLIMBER_PISON_DEPLOY = 1;
     public static final int CLIMBER_PISON_RETRACT = 0;
+
+    // LIMIT SWITCHES
+    public static final int INTAKE_SWITCH = 0;
+    public static final int CONVEYOR_SWITCH = 1;
+    public static final int SHOOTER_SWITCH = 2;
 
     //RING LIGHT
     public static final int RING_LIGHT = 5;//tbd
--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -1,21 +1,59 @@
 package frc.robot.subsystems;
 
 import frc.robot.Constants;
+import frc.robot.controls.Driver;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.PWMSparkMax;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Conveyor extends SubsystemBase
 {
   private PWMSparkMax conveyorMotor;
+  private DigitalInput intakeSwitch;
+  private DigitalInput conveyorSwitch;
+  private DigitalInput shooterSwitch;
+  private Timer timer;
 
   public Conveyor()
   {
+    timer = new Timer();
     conveyorMotor = new PWMSparkMax(Constants.CONVEYOR_SPARK);
+    intakeSwitch = new DigitalInput(Constants.INTAKE_SWITCH);
+    conveyorSwitch = new DigitalInput(Constants.CONVEYOR_SWITCH);
+    shooterSwitch = new DigitalInput(Constants.SHOOTER_SWITCH);
+  }
+
+  public void standby()
+  {
+    if (!shooterSwitch.get())
+    {
+      if (intakeSwitch.get() && !Driver.getThrottleButton(Constants.REVERSE_CONVEYOR) && Driver.getSteerButton(Constants.SPIN_INTAKE))
+      {
+        feed();
+        timer.reset();
+        timer.start();
+        while (!conveyorSwitch.get() && timer.get() < 2);
+        timer.reset();
+        timer.start();
+        while (conveyorSwitch.get() && timer.get() < 2);
+        timer.stop();
+      }
+    }
+
+    stop();
   }
 
   public void feed()
   {
-    conveyorMotor.set(0.5);
+    if (!shooterSwitch.get())
+    {
+      conveyorMotor.set(0.75);
+    }
+    else
+    {
+      conveyorMotor.set(0);
+    }
   }
 
   public void stop()
