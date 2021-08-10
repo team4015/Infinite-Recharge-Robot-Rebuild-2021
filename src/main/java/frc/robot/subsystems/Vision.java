cb4ce529af3730491685fae0bc1fb45ef89c1193
droid-101 <49104581+droid-101@users.noreply.github.com>
2021-08-10 17:17:31 -0400

    Bounding Box
    Box is drawn around target.

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -6,15 +6,14 @@ import frc.robot.Constants;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
 
 public class Vision extends SubsystemBase
@@ -58,13 +57,29 @@ public class Vision extends SubsystemBase
         }
 
         List<MatOfPoint> contours = new ArrayList<>();
+        Point [] endPoints = new Point[2];
         
         Core.inRange(image, new Scalar(40, 0, 175), new Scalar(255, 255, 255), image);
         Imgproc.findContours(image, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+
+        endPoints = getBoxPoints(image);
         Imgproc.cvtColor(image, image, Imgproc.COLOR_GRAY2BGR);
+        Imgproc.rectangle(image, endPoints[0], endPoints[1], new Scalar(0, 0, 255));
+        
         Imgproc.drawContours(image, contours, -1, new Scalar(0, 255, 0), -1);
 
         return image;
+    }
+
+    private Point [] getBoxPoints(Mat image)
+    {
+        Rect rectangle = Imgproc.boundingRect(image);
+        Point [] endPoints = new Point[2];
+
+        endPoints[0] = rectangle.tl();
+        endPoints[1] = rectangle.br();
+
+        return endPoints;
     }
 
     public void start()
