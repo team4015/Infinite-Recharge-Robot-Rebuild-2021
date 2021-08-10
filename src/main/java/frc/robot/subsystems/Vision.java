fd373d0904754440942282553e231513ef38a934
droid-101 <49104581+droid-101@users.noreply.github.com>
2021-08-10 15:48:23 -0400

    Contour Drawing
    Processing will now draw contours and fill in the target green.

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -6,8 +6,14 @@ import frc.robot.Constants;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
@@ -31,8 +37,6 @@ public class Vision extends SubsystemBase
         CvSink videoInput = CameraServer.getInstance().getVideo();
         CvSource videoOutput = CameraServer.getInstance().putVideo("Processed", 640, 480);
 
-        System.out.println("hi");
-
         Mat unprocessedPixels = new Mat();
         
         while (true)
@@ -46,25 +50,27 @@ public class Vision extends SubsystemBase
         }
     }
 
-    private Mat processFrames(Mat unprocessedPixels)
+    private Mat processFrames(Mat image)
     {
         if (!runFeed)
         {
-            return unprocessedPixels;
+            return image;
         }
 
-        Mat processedPixels = new Mat();
+        List<MatOfPoint> contours = new ArrayList<>();
+        
+        Core.inRange(image, new Scalar(40, 0, 175), new Scalar(255, 255, 255), image);
+        Imgproc.findContours(image, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+        Imgproc.cvtColor(image, image, Imgproc.COLOR_GRAY2BGR);
+        Imgproc.drawContours(image, contours, -1, new Scalar(0, 255, 0), -1);
 
-        Imgproc.cvtColor(unprocessedPixels, processedPixels, Imgproc.COLOR_BGR2HSV);
-        Core.inRange(processedPixels, new Scalar(40, 0, 175), new Scalar(255, 255, 255), processedPixels);
-
-        return processedPixels;
+        return image;
     }
+
     public void start()
     {
         ringlight.set(true);
         runFeed = true;
-        
     }
 
     public void stop()
@@ -72,6 +78,4 @@ public class Vision extends SubsystemBase
         ringlight.set(false);
         runFeed = false;
     }
-
-    
 }
